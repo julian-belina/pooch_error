2308d4c696e2b3702be3a78e9a4db4ebd91f5e57
j.belina <j.belina@fz-juelich.de>
2025-10-29 16:41:14 +0100

    run test

---
+++ get_data_from_zenodo.py
@@ -0,0 +1,15 @@
+import pooch
+from pooch import Unzip
+bathymetry_data_handler = pooch.create(
+    path=data_directory, base_url="doi:10.5281/zenodo.17047388", registry=None, version_dev=None, retry_if_failed=5
+)
+
+# Automatically populate the registry so there is no need
+# to register the files and hashes manually.
+bathymetry_data_handler.load_registry_from_doi()
+
+bathymetry_rasters_dir = str(data_directory.joinpath("gebco_tiles"))
+# Fetch one of the files in the repository
+unpack_zones_archive = Unzip(extract_dir=bathymetry_rasters_dir)
+# Download and cache the offshore gebco dataset
+bathymetry_data_handler.fetch("gebco_tiles.zip", processor=unpack_zones_archive)
